7fcd587578dab6117b7edd0f5e2f828b813dbfaf
Amit Sahu <amitsahu94156@gmail.com>
2026-09-16 02:38:15 +0530

    Add PDF notes and fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mca-notes-portal/',
-})
+
+  base: "/mca-notes-portal/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        updates: resolve(__dirname, "updates.html"),
+      },
+    },
+  },
+});
